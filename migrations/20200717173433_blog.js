2f1c0c43f443e5f9a530b4acc6b775ab03f1bf8e
brett <brett_matsumoto@yahoo.com>
2020-07-19 13:19:03 -1000

    adjusted migrations file

--- migrations/20200717173433_blog.js
+++ migrations/20200717173433_blog.js
@@ -4,11 +4,11 @@ exports.up = function (knex) {
         table.increments('id');
         table.string('title', 255).notNullable();
         table.text('content');
-        table.timestamp('created_at');
-        table.timestamp('updated_at');
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
     })
 };
 
 exports.down = function (knex) {
-
+    return knex.schema.dropTable('blog');
 };
