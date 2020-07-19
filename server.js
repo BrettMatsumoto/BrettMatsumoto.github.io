b8a70d7f38c01bef3f564a42beaf1b384ab4cfd9
brett <brett_matsumoto@yahoo.com>
2020-07-19 13:55:23 -1000

    added route for index page

--- server.js
+++ server.js
@@ -5,17 +5,15 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const blog = require('./routes/blog');
+const blogRoute = require('./routes/blog');
+const indexRoute = require('./routes/index');
 
 require('dotenv').config({ path: './.env' });
 
 const PORT = process.env.EXPRESS_CONTAINER_PORT;
 
-app.get('/', (req, res) => {
-    res.send('smoke test');
-});
-
-app.use('/blog', blog);
+app.use('/', indexRoute)
+app.use('/blog', blogRoute);
 
 app.listen(PORT, () => {
     console.log(`Express app is running on port ${PORT}.`);
