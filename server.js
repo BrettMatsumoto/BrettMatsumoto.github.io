a1f3c3f2d1894ae668e77d2560f0132fc0f4d0ba
brett <brett_matsumoto@yahoo.com>
2020-07-19 14:38:51 -1000

    added boiler plate hbs code

--- server.js
+++ server.js
@@ -2,9 +2,20 @@
 
 const knex = require('knex');
 const express = require('express');
-const app = express();
+const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 
+const app = express();
+app.use(express.static('public'));
+app.use(methodOveride('_method'));
+app.engine('.hbs', exphbs({
+    extname: '.hbs',
+    partialsDir: __dirname + '/views/partials/',
+    layoutsDir: __dirname + '/views/templates/'
+}));
+app.set('view engine', '.hbs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 const blogRoute = require('./routes/blog');
 const indexRoute = require('./routes/index');
 
