2d17bb1bbf66b24e44542ed3bfacfaf3d9b64b22
brett <brett_matsumoto@yahoo.com>
2020-07-19 14:29:36 -1000

    tested index route, need to figure out how to render html

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,8 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../database');
 
-router
-    .get('/', (req, res) => {
-        res.render('/index.html');
-    });
+router.get('/', (req, res) => {
+    res.send('index smoke test')
+})
 
 module.exports = router;
