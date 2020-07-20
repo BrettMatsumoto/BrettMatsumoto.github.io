b41eaf1b30e62adfd89ea49a669c38bd6036c6e2
brett <brett_matsumoto@yahoo.com>
2020-07-19 15:03:34 -1000

    renamed index.hbs to home and reconfigured route

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const knex = require('../database');
 
 router.get('/', (req, res) => {
-    res.render('index.hbs')
+    res.render('home.hbs')
 })
 
 module.exports = router;
